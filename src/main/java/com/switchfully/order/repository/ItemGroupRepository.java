93d593dcc7094569c620f39dc2e980264a60e2b6
jrnsmisrt <jeroen.smissaert@gmail.com>
2021-12-07 22:00:45 +0100

    filled itemgroupRepo with dummy itemgroups

--- src/main/java/com/switchfully/order/repository/ItemGroupRepository.java
+++ src/main/java/com/switchfully/order/repository/ItemGroupRepository.java
@@ -12,10 +12,25 @@ public class ItemGroupRepository {
 
     public ItemGroupRepository(){
         this.listOfItemGroups=new ArrayList<>();
+        fillRepositoryWithDummyItemGroups();
     }
 
     public List<ItemGroup> getListOfItemGroups(){
         return listOfItemGroups;
+    }
+
+    private void fillRepositoryWithDummyItemGroups(){
+       ItemGroup dummyItemGroup1 = new ItemGroup("DMMYITM1", 2);
+       ItemGroup dummyItemGroup2 = new ItemGroup("DMMYITM2", 1);
+       ItemGroup dummyItemGroup3 = new ItemGroup("DMMYITM3", 6);
+       ItemGroup dummyItemGroup4 = new ItemGroup("DMMYITM4", 5);
+       ItemGroup dummyItemGroup5 = new ItemGroup("DMMYITM5", 20);
+
+       listOfItemGroups.add(dummyItemGroup1);
+       listOfItemGroups.add(dummyItemGroup2);
+       listOfItemGroups.add(dummyItemGroup3);
+       listOfItemGroups.add(dummyItemGroup4);
+       listOfItemGroups.add(dummyItemGroup5);
     }
 
 }
