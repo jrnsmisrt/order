0965d7b0000048fba7242ea5981a0eb133dc2235
jrnsmisrt <jeroen.smissaert@gmail.com>
2021-12-07 21:53:11 +0100

    filled itemRepo with dummy items

--- src/main/java/com/switchfully/order/repository/ItemRepository.java
+++ src/main/java/com/switchfully/order/repository/ItemRepository.java
@@ -11,6 +11,7 @@ public class ItemRepository {
 
     public ItemRepository(){
         this.listOfItems = new ArrayList<>();
+        fillRepositoryWithDummyItems();
     }
 
     public void addItem(Item item){
@@ -19,6 +20,19 @@ public class ItemRepository {
 
     public List<Item> showListOfItems(){
         return listOfItems;
+    }
+
+    private void fillRepositoryWithDummyItems() {
+        Item dummyItem1 = new Item("DummyItem1", "dummy dummy", 25, 100);
+        Item dummyItem2 = new Item("DummyItem2", "dummy dummy", 30, 1000);
+        Item dummyItem3 = new Item("DummyItem3", "dummy dummy", 45, 10000);
+        Item dummyItem4 = new Item("DummyItem4", "dummy dummy", 60, 10);
+        Item dummyItem5 = new Item("DummyItem5", "dummy dummy", 99, 1);
+        listOfItems.add(dummyItem1);
+        listOfItems.add(dummyItem2);
+        listOfItems.add(dummyItem3);
+        listOfItems.add(dummyItem4);
+        listOfItems.add(dummyItem5);
     }
 
 }
