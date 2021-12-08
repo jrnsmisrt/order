02d0173679ac5f23a0cc7f0204ad0b930acabe2d
jrnsmisrt <jeroen.smissaert@gmail.com>
2021-12-08 09:48:38 +0100

    added UserServiceTest createCustomerAccount

---
+++ src/test/java/com/switchfully/order/service/UserServiceTest.java
@@ -0,0 +1,27 @@
+package com.switchfully.order.service;
+
+import com.switchfully.order.domain.user.User;
+import com.switchfully.order.repository.UserRepository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest
+@DirtiesContext(classMode=DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+
+public class UserServiceTest {
+    @Autowired
+    UserService userService;
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    void whenCreatingNewCustomerAccount_AssureCustomerIsAddedToRepository(){
+        User testUser = new User("TestUser","testUser", "@@@", "ttt", "000");
+        userService.createCustomerAccount(testUser);
+        Assertions.assertThat(userRepository.showAllUsers()).contains(testUser);
+    }
+}
