c52be5bb6d03b599f60db2ecdbc52c88d683317a
jrnsmisrt <jeroen.smissaert@gmail.com>
2022-01-04 16:34:34 +0100

    itemgrouprepo adjustment

--- src/main/java/com/switchfully/order/repository/ItemGroupRepository.java
+++ src/main/java/com/switchfully/order/repository/ItemGroupRepository.java
@@ -2,6 +2,8 @@ package com.switchfully.order.repository;
 
 import com.switchfully.order.domain.item.Item;
 import com.switchfully.order.domain.item.ItemGroup;
+import com.switchfully.order.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -9,29 +11,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-public class ItemGroupRepository {
-    private List<ItemGroup> listOfItemGroups;
-    private final ItemRepository itemRepository;
-
-    public ItemGroupRepository(ItemRepository itemRepository) {
-        this.listOfItemGroups = new ArrayList<>();
-        fillRepositoryWithDummyItemGroups();
-        this.itemRepository=itemRepository;
-    }
-
-    public List<ItemGroup> getListOfItemGroups(){
-        return listOfItemGroups;
-    }
-
-    private void fillRepositoryWithDummyItemGroups() {
-        LocalDate shippingDate = LocalDate.now();
-        Item dummyItem1 = new Item("dummyItem1","dummy",10,2);
-        ItemGroup dummyItemGroup1 = new ItemGroup(dummyItem1, shippingDate.plusDays(1), 2);
-        ItemGroup dummyItemGroup2 = new ItemGroup(dummyItem1, shippingDate.plusDays(1), 10);
-
-        listOfItemGroups.add(dummyItemGroup1);
-        listOfItemGroups.add(dummyItemGroup2);
-
-    }
+public interface ItemGroupRepository extends JpaRepository<ItemGroup, Long> {
+//    private List<ItemGroup> listOfItemGroups;
+//    private final ItemRepository itemRepository;
+//
+//    public ItemGroupRepository(ItemRepository itemRepository) {
+//        this.listOfItemGroups = new ArrayList<>();
+//        fillRepositoryWithDummyItemGroups();
+//        this.itemRepository=itemRepository;
+//    }
+//
+//    public List<ItemGroup> getListOfItemGroups(){
+//        return listOfItemGroups;
+//    }
+//
+//    private void fillRepositoryWithDummyItemGroups() {
+//        LocalDate shippingDate = LocalDate.now();
+//        Item dummyItem1 = new Item("dummyItem1","dummy",10,2);
+//        ItemGroup dummyItemGroup1 = new ItemGroup(dummyItem1, shippingDate.plusDays(1), 2);
+//        ItemGroup dummyItemGroup2 = new ItemGroup(dummyItem1, shippingDate.plusDays(1), 10);
+//
+//        listOfItemGroups.add(dummyItemGroup1);
+//        listOfItemGroups.add(dummyItemGroup2);
+//
+//    }
 
 }
